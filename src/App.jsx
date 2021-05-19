3fd2e9c92c783266d54fa7597c8a9ed1b270eb5a
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2021-05-19 20:09:39 +0530

    🐛 Logic Fix of timerMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Stopwatch from './views/Stopwatch'
 import Timer from './views/Timer'
 
@@ -8,12 +8,20 @@ export default function App() {
     return (
         <div className="app">
             <div className="menu-bar">
-                <div className={!timerMode? "active" : null} onClick={() => setTimerMode(false)}>Timer</div>
-                <div className={timerMode? "active" : null} onClick={() => setTimerMode(true)}>Stopwatch</div>
+                <div
+                    className={!timerMode ? "active" : null}
+                    onClick={() => setTimerMode(true)}>
+                    Timer
+                    </div>
+                <div
+                    className={timerMode ? "active" : null}
+                    onClick={() => setTimerMode(false)}>
+                    Stopwatch
+                </div>
             </div>
             {/* <button className="timer-stopwatch-toggle" onClick={() => setTimerMode(prevState => !prevState)}>{timerMode ? "Timer":"Stopwatch"}</button> */}
-            {timerMode ?   <Stopwatch /> : <Timer />  }
-            
+            {timerMode ?  <Timer /> : <Stopwatch /> :}
+
         </div>
     )
 }
